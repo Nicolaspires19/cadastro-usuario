1d67507d41e14f0392e028f3503bd314f3ef3fe4
NicolasPires <nico19pires@gmail.com>
2025-09-05 00:30:09 -0300

    Initial commit

---
+++ src/main/java/com/ads/cadastro_usuario/infrastructure/repository/entitys/Usuario.java
@@ -0,0 +1,25 @@
+package com.ads.cadastro_usuario.infrastructure.repository.entitys;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Table(name = "usuario")
+@Entity
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+
+    @Column(name = "email", unique = true)
+    private String email;
+
+    @Column(name = "nome")
+    private String nome;
+
+}
